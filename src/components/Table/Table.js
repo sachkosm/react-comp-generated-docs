5d94d12c0f02c9033a728982ce7b2ea0a3ccf71b
si180@yahoo.com <si180@yahoo.com>
2018-08-05 18:52:16 -0400

    demo

---
+++ src/components/Table/Table.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Table extends React.Component{
+
+
+
+  getWidthAsPercentOfTotalWidth = () => {
+    return parseInt(this.props.width * (this.props.percent / 100), 10);
+  }
+
+  render() {
+    const {width, height} = this.props;
+    return (
+      <div style={{border: 'solid 1px lightgray', width: width}}>
+        <div style={{
+          width: this.getWidthAsPercentOfTotalWidth(),
+          height: height
+        }} >Table Example</div>
+      </div>
+    );
+  }
+
+}
+
+Table.propTypes = {
+
+    /** Bar width */
+    width: PropTypes.number.isRequired,
+
+    /** Bar height */
+    height: PropTypes.number
+};
+
+Table.defaultProps = {
+    height: 5
+};
+
+export default Table;
---
+++ src/docs/examples/Table/ExampleTable.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Table from 'ps-react/Table/';
+
+export default class ExampleTable extends React.Component {
+
+    render() {
+        return <Table width='10' height='10' />
+    }
+}
